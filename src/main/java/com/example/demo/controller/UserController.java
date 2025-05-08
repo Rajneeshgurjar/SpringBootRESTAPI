a540dcd308673066cce911a0f193e7830f3703bf
rajneeshgurjar2000@gmail.com <rajneeshgurjar2000@gmail.com>
2025-05-08 16:17:25 +0530

    added ResponseEntity return type of create user and handle NoResourceFoundException using Exception Handler

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +25,9 @@ public class UserController {
 	UserServiceImpl userService;
 	
 	@PostMapping("/register")
-	public User createUser(@RequestBody User user) {
-		return userService.createUser(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		user =userService.createUser(user);
+		return new ResponseEntity<User> (user,HttpStatus.ACCEPTED);
 	}
 	
 	@GetMapping("/all")
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.demo.exception;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.example.demo.utiliy.ResponseStatus;
 
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseStatus handleUserNotFOundException(UserNotFoundException ex){
 		return new ResponseStatus(200,ex.getMessage());
+	}
+	
+	@ExceptionHandler(NoResourceFoundException.class)
+	public String resourceNotFound(NoResourceFoundException e) {
+		return "Somthing went Wrong";
 	}
 }
